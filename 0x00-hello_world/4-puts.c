0a0538ff186aed4e9a0c92017df0639150ae74cc
JesseFantasy <jessefantasy69@gmail.com>
2023-07-13 09:28:03 -0700

    added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Developed by Jesse Egwuonwu
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
