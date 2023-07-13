3a9c2d3ff0d9bd0ab3a82851cad0416cf4bb3b41
JesseFantasy <jessefantasy69@gmail.com>
2023-07-13 09:41:47 -0700

    added

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Writte by Jesse Egwuonwu
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
