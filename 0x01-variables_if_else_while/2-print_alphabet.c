abfa7218814403968fa4507c6535341a1a612684
JesseFantasy <jessefantasy69@gmail.com>
2023-07-14 10:51:59 -0700

    added

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,28 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    /* Declare a variable and initialize it with the first member of 
-       the range [a - z] 
-    */
+    char c;
 
-    char alphabet = 'a';
-
-     /* check to see if the current value of your variable is less than 
-         or the same as the last member (z) of the given range. While 
-         the value is less, go ahead and print the value 
-      */
-
-    while (alphabet <= 'z')
-    {
-        // print the value of the variable 
-        putchar(alphabet);
-
-        // increment the variable (ie: a becomes b)
-        alphabet++;
-    }
-
+    for (c = 'a'; c <= 'z'; c++)
+        putchar(c);
+    for (c = 'A'; c <= 'Z'; c++)
+        putchar(c);
     putchar('\n');
     return (0);
 }
-
