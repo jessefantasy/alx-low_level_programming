3810e7f1b1a59b2f6ae2d0a3abae43dda9eb2abb
JesseFantasy <jessefantasy69@gmail.com>
2023-07-18 11:46:21 -0700

    added

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,17 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * _isalpha - determines whether a chracter is part of the alphabet
- * @c: this is the character to be tested.
+ * _isalpha - checks if character is a letter
+ *            both lowercase or uppercase
  *
- * Return: returns 1 if character is part of the alphabet.
- * Otherwise it returns 0.
- */
+ * @c: takes input from other functions.
+ *
+ * Return: 1 is c if true else 0
+*/
+
 int _isalpha(int c)
 {
-        if (c >= 65 && c <= 122)
-                return (1);
-        return (0);
+	if (c >= 65 && c <= 122)	    
+		return (1);
+	return (0);
 }
